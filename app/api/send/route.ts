6ca0a51cb7b0f572756617964c8ff728651c95d3
sharly-jave <carlos.carrero@javeriana.edu.co>
2024-04-29 18:46:03 -0500

    Email, QR y PDF actualizados y funcionales

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -5,17 +5,17 @@ import { Resend } from 'resend';
 const resend = new Resend('re_Duyu2L8r_6LhNsvstRoenaqU7mcB2qHwm');
 
 export async function POST() {
-    try {
-      const data = await resend.emails.send({
-        from: 'Acme <onboarding@resend.dev>',
-        to: ['fervusen13@gmail.com'],
-        subject: 'Tus tiquetes por TripMakers están listos!',
-        react: EmailTemplate({ firstName: 'Carlos Andrés Carrero' }),
-        text: 'Hello world'
-      });
-  
-      return Response.json(data);
-    } catch (error) {
-      return Response.json({ error });
-    }
+  try {
+    const data = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['fervusen13@gmail.com'],
+      subject: 'Tus tiquetes por TripMakers están listos!',
+      react: EmailTemplate({ nombreCompleto: 'Carlos Andrés Carrero'}),
+      text: 'Hello world'
+    });
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error });
   }
+}
