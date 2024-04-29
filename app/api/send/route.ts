9fe359f2b20def5a717a18be4bb84c0648f527ea
sharly-jave <carlos.carrero@javeriana.edu.co>
2024-04-29 10:35:36 -0500

    Paquete "resend" añadido como dependencia en package.json y manejar el envío del correo en DetallesContacto.tsx en ShoppingCart

---
+++ app/api/send/route.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { EmailTemplate } from '@/components/EmailTemplate';
+import { Resend } from 'resend';
+
+const resend = new Resend('re_Duyu2L8r_6LhNsvstRoenaqU7mcB2qHwm');
+
+export async function POST() {
+    try {
+      const data = await resend.emails.send({
+        from: 'Acme <onboarding@resend.dev>',
+        to: ['fervusen13@gmail.com'],
+        subject: 'Tus tiquetes por TripMakers están listos!',
+        react: EmailTemplate({ firstName: 'Carlos Andrés Carrero' }),
+        text: 'Hello world'
+      });
+  
+      return Response.json(data);
+    } catch (error) {
+      return Response.json({ error });
+    }
+  }
